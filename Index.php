17296bbe683f41805641040661ad1222c808c6f3
Matthew miller <m177illerdaggeroftruth@gmail.com>
2016-01-30 10:24:51 -0500

    Update Index.php
    Wp index base temp

--- Index.php
+++ Index.php
@@ -5,8 +5,12 @@ Get_header();
 
  If (have_posts()):
    While (have_posts()) : the_post(); ?>
-     <h2><a href="<?php the_permalink()";><?php the_title();?><</a></h2>
+    
+
+   <article class="post"> 
+<h2><a href="<?php the_permalink()";><?php the_title();?><</a></h2>
       <?php the_content();?>
+  </article>
 
  <?php End while:
 
