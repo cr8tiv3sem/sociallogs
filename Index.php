8dc38db77f320e1ca178fa39b34823b434a3797e
Matthew miller <m177illerdaggeroftruth@gmail.com>
2016-01-30 08:44:38 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,19 @@
-<h1>hello world</h1>
+<?php
+ 
+Get_header();
+ 
+
+ If (have_posts()):
+   While (have_posts()) : the_post(); ?>
+     <h2><a href="<?php the_permalink()";><?php the_title();?><</a></h2>
+      <?php the_content();?>
+
+ <?php End while:
+
+  Else:
+  Echo '<p> No content found </p>'
+ 
+   End if;
+
+Get_footer();
+?>
